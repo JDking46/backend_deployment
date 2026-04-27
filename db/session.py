09048eff195774e1d77a8d289a0b5bd96a464e9e
DHARVIN <152837285+JDking46@users.noreply.github.com>
2026-04-27 22:19:51 +0530

    backend

--- db/session.py
+++ db/session.py
@@ -9,7 +9,10 @@ load_dotenv()
 database_url = os.getenv("DATABASE_URL")
 if database_url is None:
 	raise ValueError("DATABASE_URL environment variable is not set")
-engine = create_engine(database_url)
+if database_url.startswith("sqlite"):
+    engine = create_engine(database_url, connect_args={"check_same_thread": False})
+else:
+    engine = create_engine(database_url)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
